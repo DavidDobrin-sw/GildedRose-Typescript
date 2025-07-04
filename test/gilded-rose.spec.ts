80cf477d9fd5640a50c69033e67e1c13637a12c4
Dobrin David <David.Dobrin@softwire.com>
2025-07-04 15:53:13 +0300

    Added 3 tests for Backstage passes item

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -43,3 +43,47 @@ describe('Aged Brie ', function () {
 
 });
 
+describe('Backstage Passes', function () {
+
+    let gildedRose: GildedRose;
+
+    before(function () {
+        gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10)]);
+    })
+
+
+    it('I expect the Backstage passes quality to increase after one day', function() {
+        const items = gildedRose.updateQuality();
+        const passesAfterOneDay: Item = new Item('Backstage passes to a TAFKAL80ETC concert', 14, 11);
+        expect(passesAfterOneDay.quality).to.eql(gildedRose.items[0].quality);
+        expect(passesAfterOneDay.sellIn).to.eql(gildedRose.items[0].sellIn);
+    });
+
+    it('I expect the quality of the Backstage passes to increase by 2 in the final 10 days', function (){
+        const items = gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+
+        const passesAfterOneDay: Item = new Item('Backstage passes to a TAFKAL80ETC concert', 9, 17); // 17 = 10 + 5(after the first 5 days) + 2 (10th day till concert)
+
+        expect(passesAfterOneDay.quality).to.eql(gildedRose.items[0].quality);
+        expect(passesAfterOneDay.sellIn).to.eql(gildedRose.items[0].sellIn);
+    });
+
+    it('I expect the Backstage passes quality to increase by 3 in the final 5 days', function() {
+        const items = gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+        gildedRose.updateQuality();
+
+        const passesAfterOneDay: Item = new Item('Aged Brie', 4, 28); // 23 = 10 + 5(after the first 5 days) + 5*2(days 10-6) + 3 (fifth final day)
+
+        expect(passesAfterOneDay.quality).to.eql(gildedRose.items[0].quality);
+        expect(passesAfterOneDay.sellIn).to.eql(gildedRose.items[0].sellIn);
+    });
+
+});
+
